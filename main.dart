fad138f340ee5f766e1fcec84c31f9f47ba0a1cd
maryamal19 <alhaifamaryam807@gmail.com>
2024-11-28 09:03:58 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,86 +1,139 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Flutter Layout Demo'),
-          centerTitle: true,
+          title: Text('Strawberry Smoothis'),
         ),
-        body: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Image.asset(
-              'images/mekah.jpg',
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Travelhaifa',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+        body: Row(
+          children: [
+            // Column on the left
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: const Color.fromARGB(255, 235, 135, 201),
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'dilyams smots',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'tangerang selatan, Indonesia',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+                    SizedBox(height: 16),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: const Color.fromARGB(255, 255, 169, 226)),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Strawberry Smoothis',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Salah satu olahan strawberry yang bisa kamu coba adalah strawberry smoothies, yakni strawberry yang diblender bersama air, susu, atau yoghurt. Tak hanya itu, smoothies ini juga bisa dilengkapi dengan buah lain, kacang, atau biji-bijian seperti chia seed'
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            children: [
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              SizedBox(width: 8),
+                              Text('1M Reviews'),
+                            ],
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.red),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Icon(Icons.kitchen, color: Colors.red),
+                                    SizedBox(height: 8),
+                                    Text('PREP:'),
+                                    Text('25 min'),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.green),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Icon(Icons.timer, color: Colors.green),
+                                    SizedBox(height: 8),
+                                    Text('COOK:'),
+                                    Text('1 hr'),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: 16),
+                              Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.orange),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Icon(Icons.restaurant, color: Colors.orange),
+                                    SizedBox(height: 8),
+                                    Text('FEEDS:'),
+                                    Text('4-6'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                children: [
-                  const Icon(Icons.star, color: Colors.amber),
-                  const SizedBox(width: 8),
-                  const Text('5.1'),
-                  const SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.phone),
-                    label: const Text('CALL'),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.navigation),
-                    label: const Text('ROUTE'),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.share),
-                    label: const Text('SHARE'),
-                  ),
-                ],
+            // Image on the right
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'images/download.jpg', // Update this path with your image asset
+                  fit: BoxFit.cover, // Adjust the image to cover the available space
+                ),
               ),
             ),
-
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Ayo untuk Anda yang ingin menjalankan ibadah umroh khusyuk ke Tanah Suci bisa menggunakan jasa travel umroh kami. Pergi umroh dengan aman, nyaman dan menyenangkan hanya di Travel Umroh Andalan Ummat”.,.',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ]),
+          ],
         ),
       ),
     );
